fa20449a93ca7468fc5a55fd556a9a37c460725b
Bert Wagner <github@bertwagner.com>
2018-05-02 21:56:29 -0400

    Update JSONtoSQLGenerator.sql

--- JSONtoSQLGenerator.sql
+++ JSONtoSQLGenerator.sql
@@ -7,15 +7,19 @@ It is not perfect, but should provide a decent starting point when starting
 to work with new JSON schemas.
 */
 
-DECLARE @JsonData nvarchar(max) = '
-{
-	"id" : 1,
-	"IsActive":true,
-	"people" : ["Jim","Joan","John","Jeff"],
-	"places" : [{"State":"Connecticut", "Capitol":"Hartford"},{"State":"Ohio","Capitol":"Columbus","MajorCities":["Cleveland","Cincinnati"]}],
-	"created_at":"2018-04-18T21:25:48Z"
-}';
-DECLARE @RootTableName nvarchar(4000) = N'AppInstance';
+DECLARE 
+	@JsonData nvarchar(max) = '
+		{
+			"Id" : 1,
+			"IsActive":true,
+			"People" : ["Jim","Joan","John","Jeff"],
+			"Places" : [{"State":"Connecticut", "Capitol":"Hartford"},{"State":"Ohio","Capitol":"Columbus","MajorCities":["Cleveland","Cincinnati"]}],
+			"Created_At":"2018-04-18T21:25:48Z"
+		}',
+	@RootTableName nvarchar(4000) = N'AppInstance',
+	@DefaultStringType nvarchar(100) = 'nvarchar',
+	@DefaultStringPadding smallint = 20;
+
 
 WITH jsonRoot AS (
 	SELECT 0 as parentLevel, 0 AS level, [type],
