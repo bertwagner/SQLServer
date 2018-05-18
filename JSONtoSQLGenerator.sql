d98944561cc62dd572796c72a4cf01316254022b
Bert Wagner <github@bertwagner.com>
2018-05-18 11:52:35 -0400

    Cleans up code

--- JSONtoSQLGenerator.sql
+++ JSONtoSQLGenerator.sql
@@ -1,21 +1,12 @@
 /*
 This code takes a JSON input string and automatically generates
-SQL Server CREATE TABLE and INSERT INTO statements to make it easier
-to convert serialized data into tables.
+SQL Server CREATE TABLE statements to make it easier
+to convert serialized data into a database schema.
 
 It is not perfect, but should provide a decent starting point when starting
-to work with new JSON schemas.
+to work with new JSON files.
 
-Make Id, unless Id column already exists - assumption!
-Arrays will be given column names same as table name
-Must get Id PK of parent record
-
-DOes not consider numeric types past int vs float
-Is it perfect?  No.  But sure beats having to define this all by hand.
-
-Quick (somewhat) and dirty (very)
-
-CREATE STRING_AGG polyfill function for older versions of SQL
+A blog post with more information can be found at https://bertwagner.com/2018/05/22/converting-json-to-sql-server-create-table-statements/
 */
 SET NOCOUNT ON;
 
@@ -37,28 +28,58 @@ DECLARE
 
 DROP TABLE IF EXISTS ##parsedJson;
 WITH jsonRoot AS (
-	SELECT 0 as parentLevel, CONVERT(nvarchar(4000),NULL) COLLATE Latin1_General_BIN2 as parentTableName , 0 AS level, [type] ,
-	@RootTableName COLLATE Latin1_General_BIN2 AS TableName,
-	[key] COLLATE Latin1_General_BIN2 as ColumnName,
-	[value],
-	ROW_NUMBER() OVER (ORDER BY (SELECT 1)) AS ColumnSequence
-	FROM OPENJSON(@JsonData, '$')
+	SELECT 
+		0 as parentLevel, 
+		CONVERT(nvarchar(4000),NULL) COLLATE Latin1_General_BIN2 as parentTableName, 
+		0 AS [level], 
+		[type] ,
+		@RootTableName COLLATE Latin1_General_BIN2 AS TableName,
+		[key] COLLATE Latin1_General_BIN2 as ColumnName,
+		[value],
+		ROW_NUMBER() OVER (ORDER BY (SELECT 1)) AS ColumnSequence
+	FROM 
+		OPENJSON(@JsonData, '$')
 	UNION ALL
-	SELECT jsonRoot.level as parentLevel, CONVERT(nvarchar(4000),jsonRoot.TableName) COLLATE Latin1_General_BIN2, jsonRoot.level+1, d.[type],
-	CASE WHEN jsonRoot.type IN (4,5) THEN CONVERT(nvarchar(4000),jsonRoot.ColumnName) ELSE jsonRoot.TableName END COLLATE Latin1_General_BIN2,
-	CASE WHEN jsonRoot.type IN (4) THEN jsonRoot.ColumnName ELSE d.[key] END,
-	d.[value],
-	ROW_NUMBER() OVER (ORDER BY (SELECT 1)) AS ColumnSequence
-	FROM jsonRoot
-	CROSS APPLY OPENJSON(jsonRoot.[value], '$') d
-	WHERE jsonRoot.[type] IN (4,5) 
+	SELECT 
+		jsonRoot.[level] as parentLevel, 
+		CONVERT(nvarchar(4000),jsonRoot.TableName) COLLATE Latin1_General_BIN2, 
+		jsonRoot.[level]+1, 
+		d.[type],
+		CASE WHEN jsonRoot.[type] IN (4,5) THEN CONVERT(nvarchar(4000),jsonRoot.ColumnName) ELSE jsonRoot.TableName END COLLATE Latin1_General_BIN2,
+		CASE WHEN jsonRoot.[type] IN (4) THEN jsonRoot.ColumnName ELSE d.[key] END,
+		d.[value],
+		ROW_NUMBER() OVER (ORDER BY (SELECT 1)) AS ColumnSequence
+	FROM 
+		jsonRoot
+		CROSS APPLY OPENJSON(jsonRoot.[value], '$') d
+	WHERE 
+		jsonRoot.[type] IN (4,5) 
 ), IdRows AS (
-	SELECT -2 as parentLevel,null as parentTableName,-1 as level,null as type,TableName as Tablename,TableName+'Id' as columnName, null as value,0 as columnsequence
-	FROM (SELECT DISTINCT tablename FROM jsonRoot) j
+	SELECT 
+		-2 as parentLevel,
+		null as parentTableName,
+		-1 as [level],
+		null as [type],
+		TableName as Tablename,
+		TableName+'Id' as columnName, 
+		null as [value],
+		0 as columnsequence
+	FROM 
+		(SELECT DISTINCT tablename FROM jsonRoot) j
 ), FKRows AS (
-	SELECT DISTINCT -1 as parentLevel,null as parentTableName,-1 as level,null as type,TableName as Tablename,parentTableName+'Id' as columnName, null as value,0 as columnsequence
-	FROM (SELECT DISTINCT tableName,parentTableName FROM jsonRoot) j
-	WHERE parentTableName is not null
+	SELECT 
+		DISTINCT -1 as parentLevel,
+		null as parentTableName,
+		-1 as [level],
+		null as [type],
+		TableName as Tablename,
+		parentTableName+'Id' as columnName, 
+		null as [value],
+		0 as columnsequence
+	FROM 
+		(SELECT DISTINCT tableName,parentTableName FROM jsonRoot) j
+	WHERE 
+		parentTableName is not null
 )
 SELECT 
 	*,
@@ -82,38 +103,37 @@ INTO ##parsedJson
 FROM jsonRoot
 WHERE 
 	[type] in (1,2,3)
-UNION ALL SELECT IdRows.parentLevel, IdRows.parentTableName, IdRows.level, IdRows.type, IdRows.TableName, IdRows.ColumnName, IdRows.value, -10 AS ColumnSequence, 'int IDENTITY(1,1) PRIMARY KEY' as datatype, null as datatypeprecision FROM IdRows 
-UNION ALL SELECT FKRows.parentLevel, FKRows.parentTableName, FKRows.level, FKRows.type, FKRows.TableName, FKRows.ColumnName, FKRows.value, -9 AS ColumnSequence, 'int' as datatype, null as datatypeprecision FROM FKRows 
+UNION ALL SELECT IdRows.parentLevel, IdRows.parentTableName, IdRows.[level], IdRows.[type], IdRows.TableName, IdRows.ColumnName, IdRows.[value], -10 AS ColumnSequence, 'int IDENTITY(1,1) PRIMARY KEY' as datatype, null as datatypeprecision FROM IdRows 
+UNION ALL SELECT FKRows.parentLevel, FKRows.parentTableName, FKRows.[level], FKRows.[type], FKRows.TableName, FKRows.ColumnName, FKRows.[value], -9 AS ColumnSequence, 'int' as datatype, null as datatypeprecision FROM FKRows 
 
---SELECT * FROM ##parsedJson ORDER BY ParentLevel, level, tablename, columnsequence
+-- For debugging:
+-- SELECT * FROM ##parsedJson ORDER BY ParentLevel, level, tablename, columnsequence
 
-DECLARE
-	@CreateStatements nvarchar(max)
+DECLARE @CreateStatements nvarchar(max);
 
 SELECT
 	@CreateStatements = COALESCE(@CreateStatements + CHAR(13) + CHAR(13), '') + 
 	'CREATE TABLE ' + @Schema + '.' + TableName + CHAR(13) + '(' + CHAR(13) +
-		--ISNULL(c2 +' int,' + CHAR(13),'')+
 		STRING_AGG( ColumnName + ' ' + DataType + ISNULL('('+CAST(DataTypePrecision AS nvarchar(20))+')','') +  CASE WHEN DataType like '%PRIMARY KEY%' THEN '' ELSE ' NULL' END, ','+CHAR(13)) WITHIN GROUP (ORDER BY ColumnSequence) 
 	+ CHAR(13)+')'
 FROM
 	(SELECT DISTINCT 
-		j.TableName, j.ColumnName,p.ColumnName as c2, MAX(j.ColumnSequence) AS ColumnSequence, j.DataType, j.DataTypePrecision, j.[level] 
+		j.TableName, 
+		j.ColumnName,
+		MAX(j.ColumnSequence) AS ColumnSequence, 
+		j.DataType, 
+		j.DataTypePrecision, 
+		j.[level] 
 	FROM 
 		##parsedJson j
-		CROSS APPLY (SELECT TOP 1
-		ParentTableName + 'Id' AS ColumnName FROM ##parsedJson p WHERE j.TableName = p.TableName ) p
+		CROSS APPLY (SELECT TOP 1 ParentTableName + 'Id' AS ColumnName FROM ##parsedJson p WHERE j.TableName = p.TableName ) p
 	GROUP BY
 		j.TableName, j.ColumnName,p.ColumnName, j.DataType, j.DataTypePrecision, j.[level] 
 	) j
 GROUP BY
 	TableName
---	c2,
-
---ORDER BY
-	--[level]
 
 
-PRINT @CreateStatements
+PRINT @CreateStatements;
 
 
